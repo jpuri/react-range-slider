fce0b32292513ce8f220552b0c86c213d130fe00
jpuri <jyotipuri@gmail.com>
2016-06-30 18:33:03 +0530

    chenging default styles of slider

--- js/components/Slider/styles.js
+++ js/components/Slider/styles.js
@@ -2,11 +2,11 @@ export default{
   wrapper: {
     position: 'relative',
     width: '100%',
-    height: 22,
+    height: 34,
   },
   wrapperVertical: {
     position: 'relative',
     height: '100%',
-    width: 22,
+    width: 34,
   },
 };
